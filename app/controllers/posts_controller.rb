54554d194b4fde765950985cc4c58276fcdbb4be
takashi <nandeyanen08@gmail.com>
2020-07-17 05:21:21 +0900

    add new create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,19 @@
 class PostsController < ApplicationController
+  
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:caption)
+  end
 end

4451b04fd80f2425ea56c0beb8d23eaac97ac164
takashi <nandeyanen08@gmail.com>
2020-07-21 06:08:12 +0900

    refactor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [ :show, :edit, :update, :destroyn ]
+
   def index
     @posts = Post.order(created_at: :desc)
   end
@@ -17,15 +19,12 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to @post, notice: '投稿を更新したぞ.'
     else
@@ -34,7 +33,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path, notice: "投稿を削除しますた."
   end
@@ -44,4 +42,9 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:caption, :image)
   end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
